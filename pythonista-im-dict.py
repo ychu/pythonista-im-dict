de968b6412d483a89c35ec655b386939a75660dd
ychu <yauchong@gmail.com>
2017-06-24 21:47:51 +0800

    Add CinFile class to read cin file

---
+++ pythonista-im-dict.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import sys
+
+
+class CinFile:
+
+    def __init__(self, cin_file_name):
+        self.cname = None
+        self.__key2char = dict()
+        self.__char2key = dict()
+        self.__key2symbol = dict()
+
+        with open(cin_file_name, encoding='utf-8') as fin:
+
+            # get cname
+            for line in fin:
+                if line.startswith("%cname "):
+                    self.cname = line.rstrip().split(" ")[1]
+                    break
+
+            # bypass until keyname section
+            while (not fin.readline().startswith("%keyname begin")):
+                pass
+
+            # read keyname
+            for line in fin:
+                if line.startswith("%keyname end"):
+                    break
+                key, symbol = line.rstrip().split(" ")
+                self.__key2symbol[key] = symbol
+
+            # bypass until chardef section
+            while (not fin.readline().startswith("%chardef begin")):
+                pass
+
+            # read xhardef
+            for line in fin:
+                if line.startswith("%chardef end"):
+                    break
+                key, char = line.rstrip().split(" ")
+                if key not in self.__key2char:
+                    self.__key2char[key] = []
+                self.__key2char[key].append(char)
+                if char not in self.__char2key:
+                    self.__char2key[char] = []
+                self.__char2key[char].append(key)
+
+    def lookup(self, key):
+        if key in self.__key2char:
+            return self.__key2char[key]
+        else:
+            return None
+
+    def rlookup(self, char, symbol=True):
+        if char not in self.__char2key:
+            return None
+        res = []
+        for key in self.__char2key[char]:
+            if symbol:
+                syms = [self.__key2symbol[k] for k in list(key)]
+                res.append(''.join(syms))
+            else:
+                res.append(key)
+        return res
